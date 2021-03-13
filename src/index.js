71efa6f6c8f70a7dd4721b2e008fe754d65287fb
KristinaBychenok <kristinabychenokPR.gmail.com>
2021-03-14 02:42:34 +0300

    feat: task complet

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let result
+    if (n >= 0) {
+       result = String (n)
+    } else {result = String (n * (-1))};
+
+    let a = result.split('').reverse().join('');
+    return Number(a)
 }
+ 
